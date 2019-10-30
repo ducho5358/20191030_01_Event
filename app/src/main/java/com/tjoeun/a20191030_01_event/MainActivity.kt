697cff014db891b2b141b68d5bd79d51a39258d6
ducho <ducho5358@gmail.com>
2019-10-30 20:53:23 +0900

    이벤트 처리 연습 ArrayList 연습 조건문 연습

--- app/src/main/java/com/tjoeun/a20191030_01_event/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191030_01_event/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.a20191030_01_event
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -35,6 +36,51 @@ class MainActivity : AppCompatActivity() {
                     break@firstFor
                 }
             }
+        }
+
+
+        var stringList:ArrayList<String> = ArrayList()
+
+        // "오덕호" 글자를 추가.
+
+        stringList.add("오덕호")
+        // " 황진호" 글자를 추가.
+        stringList.add("황진호")
+        // " 구민성" 글자를 추가.
+        stringList.add("구민성")
+
+        var intList:ArrayList<Int> = ArrayList()
+        intList.add(10)
+        intList.add(20)
+        intList.add(50)
+
+
+        // 구민성은 몇번째로 넣었을까? 어느 위치에?
+
+        val indexOfKoo = stringList.indexOf("구민성")
+
+
+
+
+        var a = 10
+        var b = 20
+
+        var c = if (a=b) {
+            Log.d("로그", "둘이 같다")
+
+        } else {
+            Log.d("로그", "둘이 다르다.")
+
+        }
+
+        when(a) {
+            0, 1 -> Log.d("when연습", " a가 0이나 1이다")
+            in 2..5 -> Log.d("when연습", " a가 0이나 1이다")
+            in intList -> Log.d("when연습", " a가 0이나 1이다")
+            !in intList -> Log.d("when연습", " a가 0이나 1이다")
+
+
+
         }
 
 
